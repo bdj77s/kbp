d30a3a299e9e1b691453fe7128e96eeea6d15131
bdj77s <bdj77s@gmail.com>
2014-07-16 17:12:31 +0900

    20140716

--- src/main/java/com/bangdj/myapp/sample/controller/HomeController.java
+++ src/main/java/com/bangdj/myapp/sample/controller/HomeController.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.bangdj.myapp.sample.service.HomeService;
 
